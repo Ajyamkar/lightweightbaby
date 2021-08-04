66a04a607ee5b290ed7fd7a9c9c0a192dfeb984a
Ajay Yamkar <ajyamkar99@gmail.com>
2021-08-04 15:39:06 +0530

    fixed the issue of displaying maintaince calories

--- src/Components/Content/CalorieCounter/CalorieCounter.js
+++ src/Components/Content/CalorieCounter/CalorieCounter.js
@@ -103,6 +103,18 @@ export default class CalorieCounter extends Component {
 
         const maintenanceCalories = Math.round(BMR * activityLevelValue);
 
+        this.setState({
+            ...this.state,
+            maintenanceCalories: maintenanceCalories,
+            caloriesRequiredToBeConsumedForAMeal:{
+                Breakfast: maintenanceCalories*0.2, //20% of calories required for breakfast
+                MorningSnack: maintenanceCalories*0.05, //5% of calories required for morning Snacks & evening snacks
+                Lunch: maintenanceCalories*0.35, //35% of calories required for lunch & dinner
+                EveningSnack: maintenanceCalories*0.05,
+                Dinner: maintenanceCalories*0.35
+            }
+        })
+
         api.post('/saveMealsData/getSavedMealsData', {
             token: cookie.get('token')
         }).then(data => {
@@ -110,14 +122,7 @@ export default class CalorieCounter extends Component {
 
             this.setState({
                 ...this.state,
-                maintenanceCalories: maintenanceCalories,
-                caloriesRequiredToBeConsumedForAMeal:{
-                    Breakfast: maintenanceCalories*0.2, //20% of calories required for breakfast
-                    MorningSnack: maintenanceCalories*0.05, //5% of calories required for morning Snacks & evening snacks
-                    Lunch: maintenanceCalories*0.35, //35% of calories required for lunch & dinner
-                    EveningSnack: maintenanceCalories*0.05,
-                    Dinner: maintenanceCalories*0.35
-                },
+               
                 nutritionRow: data.data
             })
 
